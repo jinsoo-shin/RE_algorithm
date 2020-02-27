8c680cbf1fe0abb803294aea1e345660d6409dc3
jinsoo <sjins0127@gmail.com>
2020-02-27 19:20:55 +0900

    Add Main_N project

---
+++ test/src/Main_N/Main.java
@@ -0,0 +1,14 @@
+package Main_N;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("src/Main_N/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	}
+
+}
